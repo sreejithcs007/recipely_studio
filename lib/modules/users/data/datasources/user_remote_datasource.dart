6237f828307a097091b3113f1faba6360083ce39
sreejithcs007 <sreejithcs365@gmail.com>
2026-07-05 21:56:07 +0530

    feat: implement UserRemoteDataSource with Supabase query logic for fetching filtered users

--- lib/modules/users/data/datasources/user_remote_datasource.dart
+++ lib/modules/users/data/datasources/user_remote_datasource.dart
@@ -13,7 +13,7 @@ class UserRemoteDataSource {
   }) async {
     var selectQuery = _supabaseClient
         .from('users')
-        .select('*, user_roles(role)');
+        .select('*, user_roles!user_roles_user_id_fkey(role)');
     
     if (query != null && query.isNotEmpty) {
       selectQuery = selectQuery.or('name.ilike.%$query%,email.ilike.%$query%');
